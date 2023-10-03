9cce9e9593106d913ea99bab95fce71a80464c9b
Kutsal Umut Fırat <umut.firat55@gmail.com>
2023-10-03 16:46:15 +0300

    BasicApp

---
+++ CourseApp/Controllers/CandidateController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.Controllers
+{
+    public class CandidateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        
+        
+        public IActionResult Apply()
+        {
+            return View();
+        }
+    }
+}
